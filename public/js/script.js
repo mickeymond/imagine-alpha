c7a640ccb253cae4eefd0b928e31f862ad0f565e
Michael Hammond <mickeymond@gmail.com>
2024-06-11 12:09:42 +0000

    Array methods in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const myButton = document.getElementById('btn');
 myButton.onclick = function () {
-    const name = prompt('Enter your full name.');
-    document.getElementById('name').innerText = name;
+  const name = prompt('Enter your full name.');
+  document.getElementById('name').innerText = name;
 }
 
 // Variables
@@ -13,18 +13,18 @@ console.log(name);
 
 // Objects
 const car = {
-    numberPlate: 'GR-345-24',
-    model: 'Bentley',
-    color: 'pink',
-    weight: 50,
-    weightUnit: 'KG',
-    owner: {
-        name: 'Michael Hammond',
-        driver: {
-            name: 'Fiifi',
-            license: '',
-        }
+  numberPlate: 'GR-345-24',
+  model: 'Bentley',
+  color: 'pink',
+  weight: 50,
+  weightUnit: 'KG',
+  owner: {
+    name: 'Michael Hammond',
+    driver: {
+      name: 'Fiifi',
+      license: '',
     }
+  }
 }
 console.log(car.numberPlate);
 console.log(car.owner.driver.name);
@@ -37,44 +37,44 @@ console.log(car);
 
 // Arrays
 const tweet1 = {
-    text: 'I am learning js today.',
-    likes: 5,
-    shares: 2,
-    retweets: 1
-  }
-  
-  const tweet2 = {
-    text: 'I am learning arrays today.',
-    likes: 8,
-    shares: 3,
-    retweets: 0
-  }
-  
-  const tweets = [];
-  tweets.push(tweet1);
-  tweets.push(tweet2);
-  tweets.length;
-  tweets;
-  
-  // Dates
-  const today = new Date();
-  today.toString();
-  today.toLocaleDateString();
-  today.getHours();
-  
-  // If/Else
-  if (today.getHours() >= 16) {
-    console.log('Class has ended!');
-  } else {
-    console.log('Class is in session!');
-  }
-  
-  // For loop
-  for (let i = 0; i <= 9; i++) {
-    console.log('Notify friends!', i);
-  }
+  text: 'I am learning js today.',
+  likes: 5,
+  shares: 2,
+  retweets: 1
+}
 
-  // Functions
+const tweet2 = {
+  text: 'I am learning arrays today.',
+  likes: 8,
+  shares: 3,
+  retweets: 0
+}
+
+const tweets = [];
+tweets.push(tweet1);
+tweets.push(tweet2);
+tweets.length;
+tweets;
+
+// Dates
+const today = new Date();
+today.toString();
+today.toLocaleDateString();
+today.getHours();
+
+// If/Else
+if (today.getHours() >= 16) {
+  console.log('Class has ended!');
+} else {
+  console.log('Class is in session!');
+}
+
+// For loop
+for (let i = 0; i <= 9; i++) {
+  console.log('Notify friends!', i);
+}
+
+// Functions
 // Defining functions
 function login(username, password) {
   if (username == 'mickeymond' && password == '1234') {
@@ -115,7 +115,7 @@ Math.max(78, 12, 98, 14, 89);
 Math.ceil(Math.random() * 1000000);
 
 // Order of operations
-1 * 2 + 3 - 4 / 2 + 2**2;
+1 * 2 + 3 - 4 / 2 + 2 ** 2;
 
 // Handling user inputs
 1200 + Number('delete');
@@ -167,3 +167,40 @@ const alphabets = ['A', 'B', 'C', 'D'];
 alphabets;
 alphabets.join(' ');
 alphabets.join(' ').split(' ');
+
+// Write a function that will take a user with firstname, lastname and return fullname;
+const user = {
+  firstname: 'Joe',
+  lastname: 'Ganner'
+}
+function fullname(user) {
+  return {
+    ...user,
+    fullname: `${user.firstname} ${user.lastname}`
+  }
+}
+
+fullname(user);
+
+// Array map
+const users = [
+  { firstname: 'Linda', lastname: 'Parker' },
+  { firstname: 'Mark', lastname: 'Twaine' },
+  { firstname: 'Oliver', lastname: 'Queen' },
+  { firstname: 'Stephen', lastname: 'Grider' },
+  { firstname: 'Elon', lastname: 'Musk' }
+]
+users.map(fullname);
+
+// Map and return squares of numbers
+function square(number) {
+  return number ** 2;
+}
+const numbers = [9, 8, 7, 6]
+numbers.map(square);
+
+// Filter and return even numbers
+function even(number) {
+  return number % 2 === 0;
+}
+numbers.filter(even);
